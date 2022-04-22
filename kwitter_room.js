b87104db16eef936b281985715060233775eb8f1
1parakeet1 <95216234+1parakeet1@users.noreply.github.com>
2022-04-22 15:53:48 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0W35JVAKa-WO_ZGiJ-4sSi4uFQIvJKtc",
+    authDomain: "kwitterproject-140d8.firebaseapp.com",
+    databaseURL: "https://kwitterproject-140d8-default-rtdb.firebaseio.com",
+    projectId: "kwitterproject-140d8",
+    storageBucket: "kwitterproject-140d8.appspot.com",
+    messagingSenderId: "290536992597",
+    appId: "1:290536992597:web:edc776187292b7f62049dc",
+    measurementId: "G-VT1FNDP7X9"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
